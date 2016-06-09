dc9750ff3e0ba152fb01a1f2e7717860076bf3ce
morvarid mozafarian <morvarid mozafarian>
2016-06-08 20:28:34 -0700

    google map

--- angular/script.js
+++ angular/script.js
@@ -45,3 +45,64 @@ app.controller('mainController',function ($scope) {
     $scope.banner = "../assets/images/welcome-image.png";
 });
 
+///////////////////////////////////google map/////////////////////////
+var userLocation = {
+    lat : null,
+    lng: null
+} ;
+function initMap() {
+    getLocation();
+    console.log("user location1: ",userLocation);
+    var mapDiv = document.getElementById('map');
+    var store=new google.maps.LatLng(33.532029,-117.702148);
+    var user = new google.maps.LatLng(userLocation.lat,userLocation.lng);
+    console.log("userLocation.lat: ", userLocation.lat, "userLocation.long: ", userLocation.lng);
+    console.log("store: ", store, "user", user);
+
+    var trip = [store, user];
+    var map = new google.maps.Map(mapDiv, {
+        center: store,
+        zoom: 8,
+         mapTypeId:google.maps.MapTypeId.ROADMAP
+    });
+    var marker=new google.maps.Marker({
+        position:map.center,
+        animation:google.maps.Animation.BOUNCE
+    });
+    marker.setMap(map);
+    var usermarker=new google.maps.Marker({
+        position: user,
+    });
+    usermarker.setMap(map);
+    var infowindow = new google.maps.InfoWindow({
+        content:"You are here!"
+    });
+    infowindow.open(map,usermarker);
+    var flightPath=new google.maps.Polyline({
+        path:trip,
+        strokeColor:"#0000FF",
+        strokeOpacity:0.8,
+        strokeWeight:2
+    });
+    flightPath.setMap(map);
+};
+function getLocation() {
+    console.log("hello from inside getLocation()");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+};
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    userLocation = {
+        lat: lat,
+        lng: lon
+    };
+    initMap();
+    //return userLocation;
+    console.log("user location: ",userLocation);
+};
