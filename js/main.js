e8c6cd5b6c8f6f100da17f43f3491cb47eebfc37
Morasthite <Morasthite@gmail.com>
2016-07-05 20:04:25 -0700

    blah blah

--- js/main.js
+++ js/main.js
@@ -5,22 +5,28 @@
 //-------Click handlers for the login in buttons, onclick, animate and show the login forms ------//
 $(document).ready (function() {
 
-    $("#log-in-btn").click(function () {
-        $("#login-form").toggleClass('expand-form-login');
-    });
-    $("#sign-up-btn").click(function () {
-        $("#signup-form").toggleClass('expand-form-signup');
-    });
-    $("#guest-checkout-btn").click(function () {
-        $("#guestcheckout-form").toggleClass('expand-form-guest-checkout');
-    });
-    $("#signup-form-submit-btn").click(function (){
-        $("#shipto-form").toggleClass('expand-shipto-form');
-    });
-    $("#guest-checkout-form-submit-btn").click(function (){
-        $("#shipto-form").toggleClass('expand-shipto-form');
-    });
-    $("#login-form-submit-btn").click(function (){
-        $("#shipto-form").toggleClass('expand-shipto-form');
-    });
+    // $("#log-in-btn").click(function () {
+    //     $("#login-form").toggleClass('expand-form-login');
+    //     $("#signup-form").toggleClass('expand-form-signup');
+    //     $("#guestcheckout-form").toggleClass('expand-form-guest-checkout');
+    // });
+    // $("#sign-up-btn").click(function () {
+    //     $("#signup-form").toggleClass('expand-form-signup');
+    //     $("#guestcheckout-form").toggleClass('expand-form-guest-checkout');
+    //     $("#login-form").toggleClass('expand-form-login');
+    // });
+    // $("#guest-checkout-btn").click(function () {
+    //     $("#guestcheckout-form").toggleClass('expand-form-guest-checkout');
+    //     $("#signup-form").toggleClass('expand-form-signup');
+    //     $("#login-form").toggleClass('expand-form-login');
+    // });
+    // $("#signup-form-submit-btn").click(function (){
+    //     $("#shipto-form").toggleClass('expand-shipto-form');
+    // });
+    // $("#guest-checkout-form-submit-btn").click(function (){
+    //     $("#shipto-form").toggleClass('expand-shipto-form');
+    // });
+    // $("#login-form-submit-btn").click(function (){
+    //     $("#shipto-form").toggleClass('expand-shipto-form');
+    // });
 });//doc ready
