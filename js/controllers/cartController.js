9298e59866b5cf410eaee362eebea9009f994c2d
Morasthite <Morasthite@gmail.com>
2016-07-06 19:57:37 -0700

    cart updating from other pages

--- js/controllers/cartController.js
+++ js/controllers/cartController.js
@@ -3,15 +3,25 @@ app.controller('cartController',["$scope","$timeout","config","cart",function ($
    config.banner = "assets/images/contact-image.png";
    config.menuIndice = 5;
     var self = this;
-    console.log("cart_service inventory = cart.macaron_array: ", cart.macaron_array);
-    var current_order_array = [];
-    for(var i = 0; i < cart.macaron_array.length-1; i++){
+            console.log("cart_service inventory = cart.macaron_array: ", cart.macaron_array);
+    //self.currentOrderArray = [];
+    self.currentOrderArray = [{
+        amount: "",
+        description: "",
+        id: "",
+        name: "",
+        ordered:"",
+        price: "",
+        source:""
+    }];
+    for(var i = 0; i < cart.macaron_array.length; i++){
         if(cart.macaron_array[i].ordered > 0){
             console.log(cart.macaron_array[i].ordered+ " of this item has been added to the cart: ",cart.macaron_array[i]);
-            current_order_array.push(cart.macaron_array[i]);
+            self.currentOrderArray.push(cart.macaron_array[i]);
+            //console.log("currentOrderArray", self.currentOrderArray);
         }
-        console.log("current_order_array = ",current_order_array);
     }
 
-
 }]);
+
+
