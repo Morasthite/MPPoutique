6282d6f4a6de356dac9b4a74b398337da106e8ef
Morasthite <Morasthite@gmail.com>
2016-07-07 07:20:46 -0700

    removing dummy self.currentOrderArray item, working ok

--- js/controllers/cartController.js
+++ js/controllers/cartController.js
@@ -4,16 +4,16 @@ app.controller('cartController',["$scope","$timeout","config","cart",function ($
    config.menuIndice = 5;
     var self = this;
             console.log("cart_service inventory = cart.macaron_array: ", cart.macaron_array);
-    //self.currentOrderArray = [];
-    self.currentOrderArray = [{
-        amount: "",
-        description: "",
-        id: "",
-        name: "",
-        ordered:"",
-        price: "",
-        source:""
-    }];
+    self.currentOrderArray = [];
+    // self.currentOrderArray = [{
+    //     amount: "",
+    //     description: "",
+    //     id: "",
+    //     name: "",
+    //     ordered:"",
+    //     price: "",
+    //     source:""
+    // }];
     for(var i = 0; i < cart.macaron_array.length; i++){
         if(cart.macaron_array[i].ordered > 0){
             console.log(cart.macaron_array[i].ordered+ " of this item has been added to the cart: ",cart.macaron_array[i]);
--- js/controllers/macaronController.js
+++ js/controllers/macaronController.js
@@ -8,7 +8,7 @@ app.controller('macaronController', ["$scope", "$timeout", "config","cart", func
     config.banner = "assets/images/our-macarons-image.png";
     config.menuIndice = 2;
     self.inventory = cart.inventory;
-    console.log("here",cart.inventory);
+    console.log("cart inventory @ beginning of macaronController: ",cart.inventory);
     cart.inventory.then(function (response) {
         var macaron_array = [];
         macaron_array.push(response.data);
@@ -17,12 +17,12 @@ app.controller('macaronController', ["$scope", "$timeout", "config","cart", func
         for(var i=0; i<response.data.length;i++){
             self.cart.macaron_array.push(response.data[i]);
         }
-        console.log("after adding: ",cart.macaron_array);
+        console.log("cart.macaron_array after adding: ",cart.macaron_array);
     });
     //TODO:
     // api call to get inventory
     self.cart = cart;
-    console.log("cart: ",cart);
+    //console.log("cart: ",cart);
     this.add = function (macaroon) {
         if (macaroon.ordered < macaroon.amount) {
             macaroon.ordered++;
