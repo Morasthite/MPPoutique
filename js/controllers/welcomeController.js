06453449f228e2545e9667a69c68374a2767ea8f
Morasthite <Morasthite@gmail.com>
2016-07-18 09:18:33 -0700

    assets folder redo

--- js/controllers/welcomeController.js
+++ js/controllers/welcomeController.js
@@ -1,5 +1,30 @@
-/**  **/
 app.controller('welcomeController',["$scope","config",function ($scope,config) {
+//     config.banner = "assets/images/welcome-image.jpg";
+//     config.menuIndice = 1;
+// }]);
+//     config.banner = "assets/welcome/welcome-image-1.png";
     config.banner = "assets/images/welcome-image.jpg";
+    config.bannerTest =
+        '<div class="carousel slide" id="welcome-carousel" data-ride="carousel">' +
+        '    <div class="carousel-inner" role="listbox">' +
+        '       <div class="item active">' +
+        '           <img src="assets/welcome/welcome-image-1.png">' +
+        '           <div class="carousel-caption">' +
+        '               <h3>Hi Life is like a box of macarons...</h3>' +
+        '               <p>..you just never know what you gunna done get...</p>' +
+        '           </div>' +
+        '       </div>' +
+        '       <div class="item">' +
+        '               <img src="assets/welcome/welcome-image-2.png">' +
+        '           </div>' +
+        '       <div class="item">' +
+        '           <img src="assets/welcome/welcome-image-3.png">' +
+        '       </div>' +
+        '       <div class="item">' +
+        '           <img src="assets/welcome/welcome-image-4.png">' +
+        '       </div>' +
+        '   </div>' +
+        ' </div>';
+
     config.menuIndice = 1;
 }]);
--- js/services/config.js
+++ js/services/config.js
@@ -2,6 +2,26 @@
 app.factory("config",function () {
     return {
         banner:"assets/images/welcome-image.jpg",
-        menuIndice : 1
+        menuIndice : 1,
+        bannerTest: '<div class="carousel slide" id="welcome-carousel" data-ride="carousel">' +
+        '    <div class="carousel-inner" role="listbox">' +
+        '       <div class="item active">' +
+        '           <img src="assets/welcome/welcome-image-1.png">' +
+        '           <div class="carousel-caption">' +
+        '               <h3>Life is like a box of macarons...</h3>' +
+        '               <p>..you just never know what you gunna done get...</p>' +
+        '           </div>' +
+        '       </div>' +
+        '       <div class="item">' +
+        '               <img src="assets/welcome/welcome-image-2.png">' +
+        '       </div>' +
+        '       <div class="item">' +
+        '           <img src="assets/welcome/welcome-image-3.png">' +
+        '       </div>' +
+        '       <div class="item">' +
+        '           <img src="assets/welcome/welcome-image-4.png">' +
+        '       </div>' +
+        '   </div>' +
+        ' </div>'
     }
 });
