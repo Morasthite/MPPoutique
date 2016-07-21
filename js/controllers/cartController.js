d7b4d7988e37f9a5ce9edb8f78b77fd8d030b953
Morasthite <Morasthite@gmail.com>
2016-07-20 21:47:35 -0700

    working on shiptoform

--- js/controllers/cartController.js
+++ js/controllers/cartController.js
@@ -526,10 +526,11 @@ app.controller('cartController',["$scope","$http","config","cart", "invoice", "u
                 }
             ]};
         console.log("self.finalizedOrder.orderTime = ",self.finalizedOrder.orderTime,"self.finalizedOrder.orderNumber = ",self.finalizedOrder.orderNumber,"self.finalizedOrder.Cart = ",self.finalizedOrder.Cart, "cart  = ",cart );
+        
     };//self.emptyCart
 
 /** **********************  ANGULAR MDL FUNCTIONS  ********************** **/
-    
+
     $scope.newDate = new Date();
     $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
