3be581c221cd16863087ba56ca4aaa4e1f8a3498
Morasthite <Morasthite@gmail.com>
2016-07-20 17:36:17 -0700

    issue 77 fixed, update inventory after purchase

--- php/save_order.php
+++ php/save_order.php
@@ -1,7 +1,4 @@
 <?php
-/**
-
- */
 session_start();
 require_once ('mysql_connect.php');
 //Collect all Details from Angular HTTP Request.
@@ -16,14 +13,20 @@ $id = $_SESSION["id"];
 $invoice_num ="'".$_POST['orderNumber']."'";
 foreach ($data as $item) {
 ////////////sending data to db
-$quantity = intval($item['ordered']);
-$name = intval($item['id']);
-$query = "INSERT INTO `purchases`(`inventory_id`, `customer_id`, `quantity_purchased`, `invoice_number`) VALUES ($name,$id,$quantity,$invoice_num)";
-    echo "query:";
-    print_r($query);
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) > 0) {
-  while ($row = mysqli_fetch_assoc($result)) {
-   }///end of while
-}///end of if
+    $quantity = intval($item['ordered']);
+    $name = intval($item['id']);
+    $query = "INSERT INTO `purchases`(`inventory_id`, `customer_id`, `quantity_purchased`, `invoice_number`) VALUES ($name,$id,$quantity,$invoice_num)";
+    $upDateInventoryQuery = "UPDATE `inventory` SET `amount` = amount-$quantity WHERE `id` = $name";
+        echo "query:";
+        print_r($query);
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+       }///end of while
+    }///end of if
+    $updateInventoryResult = mysqli_query($conn, $upDateInventoryQuery);
+    if(mysqli_num_rows($updateInventoryResult)>0){
+        while ($row = mysqli_fetch_assoc($updateInventoryResult)) {
+        }///end of while
+    }
 }/////end of for loop
